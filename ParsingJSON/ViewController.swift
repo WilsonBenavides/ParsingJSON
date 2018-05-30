a77123bdf1b6722bbee2b5f7d80fb52c97f01e20
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-30 13:56:21 -0700

    Just Became Super Easy in Swift 4 with Decodable min 6:37

--- ParsingJSON/ViewController.swift
+++ ParsingJSON/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    struct Course {
+        let id: Int
+        let name: String
+        let link: String
+        let imageUrl: String
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            //also perhaps check response status 200 ok
+            
+            guard let data = data else { return }
+            
+            let dataAsString = String(data: data, encoding: .utf8)
+            print(dataAsString!)
+        }.resume()
+        
+        //let myCourse = Course(id: 1, name: "my course", link: "some link", imageUrl: "some image url")
+        //print(myCourse)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
